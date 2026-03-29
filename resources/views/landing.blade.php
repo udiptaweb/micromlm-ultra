492fc3b2fb3c2c2b782fc34cc271161f2fecff1f
udiptaweb <gogoiudipta000@gmail.com>
2026-03-29 10:42:19 +0530

    google ads tag

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -937,7 +937,15 @@
     </style>
 
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18048083910"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'AW-18048083910');
+</script>
 <body>
 
     <div class="bg-layer"></div>
--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -120,7 +120,15 @@
 
     @stack('styles')
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18048083910"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'AW-18048083910');
+</script>
 <body>
 
     {{-- Navbar --}}
